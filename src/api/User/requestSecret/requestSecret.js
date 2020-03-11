cbedb1e9baf88cf16b0a3bafe46c323b09cea3b3
M'sWorld <m@MsWorldui-MacBook-Pro.local>
2020-03-11 18:43:37 +0900

    modify data model

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,11 +4,12 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args) => {
-      const { username, email } = args;
+      const { email } = args;
       const loginSecret = generateSecret();
       console.log(loginSecret);
 
       try {
+        const { username } = await prisma.user({ email });
         await sendSecretMail(username, email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
